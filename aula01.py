d48ec9fdfdaa4f1db34f60969dc180931840d5cd
Dennis Gabriel <dennisgabr745@gmail.com>
2022-04-10 00:27:51 -0300

    Adicionado aula01.py
    Código usado na primeira aula do curso de Python, com alguns comentários.

---
+++ aula01.py
@@ -0,0 +1,11 @@
+#O comando print serve para imprimir algo na tela.
+print("Hello, World!")
+
+a = 2 
+b = 2.6
+c = "Dennis Gabriel Dev"
+
+#o type serve pra informar o tipo de uma determinada variável 
+print(type(a))
+print(type(b))
+print(type(c))
