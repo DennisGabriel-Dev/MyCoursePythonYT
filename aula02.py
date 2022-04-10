6e38be3ffe422ac86abd1ed0037d94bab99d61d8
Dennis Gabriel <dennisgabr745@gmail.com>
2022-04-10 00:31:35 -0300

    Adicionado aula02.py
    Código usado na aula 2 do curso de Python,  o mesmo possui comentários.

---
+++ aula02.py
@@ -0,0 +1,24 @@
+#Olá Rapeize
+#coding:utf-8
+import math
+
+soma = 5 + 4 
+soma2 = 5 -- 4 #outra forma de somar
+subtracao = 5 - 4
+multiplicacao = 5 * 4
+divisao = 10/5
+
+#exponenciacao= matha.pow(base, expoente)
+exponenciacao = math.pow(3,2)
+
+#raizQuadrada= math.sqrt(numero_que_vai_ser_tirado_a_raiz_quadrada)
+raizQuadrada = math.sqrt(16)
+
+print("Soma: ", soma)
+print("Soma 2: ", soma2)
+print("Subtração: ",subtracao)
+print("Multiplicação: ",multiplicacao)
+print("Divisão: ",divisao)
+print("Exponenciação: ", exponenciacao)
+print("Raiz Quadrada: ", raizQuadrada)
+
