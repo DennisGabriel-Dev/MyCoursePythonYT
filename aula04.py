319e1a271e58190ccef3bb6a205691c3aa8fcd20
Dennis Gabriel <dennisgabr745@gmail.com>
2022-04-11 23:34:56 -0300

    Adicionado aula04.py
    Aqui tratei a respeito de métodos em Strings.

---
+++ aula04.py
@@ -0,0 +1,14 @@
+#upper lower index replace strip split
+s = " Hello Friend! "
+
+print(s.upper())
+print("------------------------")
+print(s.lower())
+print("------------------------")
+print(s.index("o"))
+print("------------------------")
+print(s.replace("e","x"))
+print("------------------------")
+print(s.strip())
+print("------------------------")
+print(s.split())
