aa44c2210ef8e5e9548f8f70657f0c0d48e68957
Dennis Gabriel <dennisgabr745@gmail.com>
2022-04-13 22:31:20 -0300

    Adicionados comentários em aula03.py
    É importante que leia com muita atenção, e se necessário abra uma issue(pergunta/questão) caso tenha dúvidas.

--- aula03.py
+++ aula03.py
@@ -1,8 +1,16 @@
+#_*_ coding:utf-8 _*_
+b = float(input ("Digite um número real: "))#Essa variável recebe um número real 
 
-b = float(input ("Digite um número real: "))
+#A condição "if" é basicamente o "se" em português. Bom, a explicação de como ele funciona é o seguinte:
+#Ele , o "if", só é executado se a condição entre parênteses após ele for verdadeira.
 if(b>100):
     print("B é maior que 100")
 
-
+#A condição else só é executada se o if apresentado anteriormente tiver a condição falsa. 
 else:
     print("B é menor que 100") 
+
+#AVISO IMPORTANTE!!!!!!
+#MAS E SE O "b" FOR IGUAL A 100??? VEJA A PARTE 2 DESTA AULA :) 
+#A PARTE 2 SE ENCONTRA NESSE REPOSITÓRIO COMO: aula03_2.py
+#LÁ EU APRESENTO OUTRA CONDIÇÃO QUE FUNCIONA COMO O "if", MAS SÓ SE ELE FOR FALSO, CLARO.
