ab8117224ab1213342f88ebe2fef1591e4589408
Dennis Gabriel <dennisgabr745@gmail.com>
2022-08-25 14:18:54 -0300

    Criando a pasta exercicios
    Junto a pasta foram colocados dois  exercicios resolvidos.

---
+++ exercicios/exercicio-1.py
@@ -0,0 +1,12 @@
+#Faça um programa que recebendo um valor inteiro, informe se o número é positivo, negativo ou neutro.
+
+variavel = int(input("Digite um valor inteiro: "))
+
+if(variavel > 0):
+    print("O número é positivo !!")
+
+elif(variavel < 0):
+    print("O número é negativo !!")
+
+else:
+    print("O número é neutro !!")
---
+++ exercicios/exercicio-2.py
@@ -0,0 +1,11 @@
+# Faça um programa em linguagem Python que leia dois números inteiros e informe se estes são iguais ou diferentes
+
+variavel1 = int(input("Informe o primeiro valor: "))
+variavel2 = int(input("Informe o segundo valor: "))
+
+if(variavel1 == variavel2):
+    print("Os números são iguais")
+
+else:
+    print("Os números são diferentes")
+    
